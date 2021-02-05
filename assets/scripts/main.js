dcc5efef5d44d9cc12dbe4d0b48410e991089a7f
sujanrai4 <sjn.bvet5@yahoo.com>
2021-02-04 18:55:36 -0800

    m

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,7 +13,7 @@ function selectHornType() {
     if(document.getElementById("radio-air-horn").checked == true) {
         document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
         document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
-    } else if(document.querySelectorAll("#radio-car-horn").checked == true) {
+    } else if(document.getElementById("radio-car-horn").checked == true) {
         document.getElementById("sound-image").src = "./assets/media/images/car.svg";
         document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3"; 
     } else {
@@ -41,7 +41,6 @@ function numberToSlider() {
 
     document.getElementById("volume-slider").value = 
                             document.getElementById("volume-number").value;
-    console.log(document.getElementById("volume-slider").value);
     assignVolIcon();
 }
 /* 
@@ -50,17 +49,21 @@ function numberToSlider() {
 
 function assignVolIcon() {
     let numVol = document.getElementById("volume-number").value;
-    
+    let source = document.getElementById("volume-image").src 
     if(numVol == 0) {
-        document.getElementById("volume-image").src = "./assets/media/icons/volume-level-0.svg";
+         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-0.svg";
         document.getElementById("honk-btn").disabled = true;
-    } else if(numVol <= 33) {
+    } else if(numVol >= 1 && numVol <= 33) {
+        console.log(numVol);
         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-1.svg";
+        console.log(source); 
         document.getElementById("honk-btn").disabled = false;
-    } else if(numVol <= 66) {
+    } else if( numVol >= 34 && numVol <= 66) {
         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-2.svg";
         document.getElementById("honk-btn").disabled = false; 
     } else {
+        console.log(numVol);
+        console.log(source);  
         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-3.svg";
         document.getElementById("honk-btn").disabled = false;
     }
@@ -71,4 +74,35 @@ function assignVolIcon() {
 //add an input event that will trigger volume number to update in response of slider
 (document.getElementById("volume-slider")).addEventListener("change", sliderToNumber);
 
-(document.querySelectorAll('input[name="radio-sound"]')).addEventListener( selectHornType);
+(document.getElementById("audio-selection")).addEventListener("input", selectHornType);
+(document.getElementById("volume-number")).addEventListener("input", assignVolIcon);
+
+/* 
+    play audio sound
+*/
+/*function playAudio(event) {
+    event.preventDefault();
+    assignVolIcon;
+    document.getElementById("horn-sound").play();
+
+    
+}*/
+function playAudio(event) {
+    event.preventDefault();
+    let newAudio = document.createElement("AUDIO");
+    let newSrc = document.createElement("SOURCE");
+    newSrc.src = document.getElementById("horn-sound").src;
+    newAudio.volume = document.getElementById("volume-slider").value / 100;
+    newAudio.appendChild(newSrc);
+    document.body.appendChild(newAudio);
+    newAudio.play();
+    let num = document.getElementsByTagName("AUDIO");
+    console.log(num);
+
+
+    
+}
+/**
+ * create an event listener for the entire form that responds to horn button
+ */
+document.getElementById("party-horn-form").addEventListener("submit", playAudio);
