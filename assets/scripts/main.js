aa4590427deda996def17b1cb72778b21847e183
sujanrai4 <sjn.bvet5@yahoo.com>
2021-02-05 17:08:30 -0800

    m

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
+/*--------------------------------------------------------------------------------------------------*/
 /**
  * Javascript file for the party horn, required for cse110 Lab5
  * @author: Sujan Rai
@@ -10,6 +11,12 @@
  *      
  */
 
+/*---------------------------------------------------------------------------------------------------*/
+
+/**
+ * function that will select the right audio type and the audio image
+ * based on the radio button checked
+ */
 function selectHornType() {
     if(document.getElementById("radio-air-horn").checked == true) {
         document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
@@ -24,47 +31,52 @@ function selectHornType() {
 }
 
 
-/*
-    update volume number field input from volume slider
+/** 
+ * Function to update volume number field input from volume slider
 */
 
 function sliderToNumber() {
     document.getElementById("volume-number").value = 
                             document.getElementById("volume-slider").value;
+    /* every time there is change in volume number input field, update the volume 
+    icon and honk button */
     assignVolIcon();
 }
 
-/* 
-    update slider volume from volume number field input
+/**
+ * Function to update slider volume from volume number field input
 */
 
 function numberToSlider() {
 
     document.getElementById("volume-slider").value = 
                             document.getElementById("volume-number").value;
+    /* every time there is change in volume slider bar, update the volume 
+    icon and honk button */
     assignVolIcon();
 }
-/* 
-    assign the correct volume icon depending on the sound level on the volume number
+/** 
+ * Function that assigns the correct volume icon depending on the sound 
+ * level on the volume number
 */
 
 function assignVolIcon() {
     let numVol = document.getElementById("volume-number").value;
-    let source = document.getElementById("volume-image").src 
-    if(numVol == 0) {
-         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-0.svg";
+    // get and the store min value of slider bar
+    let minVol = document.getElementById("volume-slider").min;
+    if(numVol <= minVol) {
+        document.getElementById("volume-image").src = "./assets/media/icons/volume-level-0.svg";
+        // set the slider bar volume to have its min value when the honk is muted
+        document.getElementById("volume-slider").value = minVol;
+        // disable the honk/make the sound muted when the volume is less than 1
         document.getElementById("honk-btn").disabled = true;
     } else if(numVol >= 1 && numVol <= 33) {
-        console.log(numVol);
         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-1.svg";
-        console.log(source); 
         document.getElementById("honk-btn").disabled = false;
     } else if( numVol >= 34 && numVol <= 66) {
         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-2.svg";
         document.getElementById("honk-btn").disabled = false; 
     } else {
-        console.log(numVol);
-        console.log(source);  
         document.getElementById("volume-image").src = "./assets/media/icons/volume-level-3.svg";
         document.getElementById("honk-btn").disabled = false;
     }
@@ -72,38 +84,32 @@ function assignVolIcon() {
 
 //add an input event that will trigger slider volume to update in response of volume number
 (document.getElementById("volume-number")).addEventListener("input", numberToSlider);
-//add an input event that will trigger volume number to update in response of slider
+//add an change event that will trigger volume number to update in response of slider
 (document.getElementById("volume-slider")).addEventListener("change", sliderToNumber);
-
+// add an input event that will trigger the selection of right audio type and audio image
 (document.getElementById("audio-selection")).addEventListener("input", selectHornType);
-(document.getElementById("volume-number")).addEventListener("input", assignVolIcon);
 
-/* 
-    play audio sound
-*/
-/*function playAudio(event) {
-    event.preventDefault();
-    assignVolIcon;
-    document.getElementById("horn-sound").play();
-
-    
-}*/
-function playAudio(event) {
-    event.preventDefault();
-    let newAudio = document.createElement("AUDIO");
-    let newSrc = document.createElement("SOURCE");
-    newSrc.src = document.getElementById("horn-sound").src;
-    newAudio.volume = document.getElementById("volume-slider").value / 100;
-    newAudio.appendChild(newSrc);
-    document.body.appendChild(newAudio);
-    newAudio.play();
-    let num = document.getElementsByTagName("AUDIO");
-    console.log(num);
-
-
-    
-}
 /**
- * create an event listener for the entire form that responds to horn button
- */
+ * Funtion that is responsible for playing an audio 
+ * sound by updating the audio tag.
+*/
+function playAudio(event) {
+    // prevent resetting back to the default settings
+    event.preventDefault();
+    // get audio tag id to update its sources and volume
+    let updatedAudio = document.getElementById("horn-sound");
+    //update source audio sound based on the radio selection
+    updatedAudio.src = document.getElementById("horn-sound").src;
+    // get the max value of volume level
+    let maxVol = document.getElementById("volume-number").max; 
+    // scale the volume level based on user choice( based on volume number and slider bar)
+    updatedAudio.volume = document.getElementById("volume-number").value / maxVol;
+    // play the updated sound
+    updatedAudio.play();
+}
+
+// create a submit event that will trigger the honk button to play audio
 document.getElementById("party-horn-form").addEventListener("submit", playAudio);
+
+
+/*************** The End ******************************************************************************/
